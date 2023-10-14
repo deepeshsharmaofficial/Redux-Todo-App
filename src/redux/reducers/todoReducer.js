ca5f98d78a733805ebb541cf54b652248b36d1b6
Deepesh Sharma <deep92112018@gmail.com>
2023-10-15 00:11:28 +0530

    Setting up Reducers

---
+++ src/redux/reducers/todoReducer.js
@@ -0,0 +1,36 @@
+import { ADD_TODO, TOGGLE_TODO } from "../actions/todoActions";
+
+// initial state
+const initialState = {
+    todos: []
+}
+
+export function todoReducer(state = initialState , action) {
+  switch(action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+              text: action.text,
+              completed: false
+          }   
+        ]
+      }
+
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: todos.map((todo, i) => {
+          if(i == action.index) {
+            todo.completed = !todo.completed;
+          }
+          return todo;
+        })
+      }
+    
+    default:
+      return state;
+  }
+}
