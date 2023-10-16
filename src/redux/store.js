daf2d0f4e41c0496f89e4d7176d635250372fa55
Deepesh Sharma <deep92112018@gmail.com>
2023-10-16 14:37:16 +0530

    Redux Toolkit : Migrating Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,10 @@
-import * as redux from "redux";
-import { combineReducers } from "redux";
 import {todoReducer} from "./reducers/todoReducer";
 import { noteReducer } from "./reducers/noteReducer";
+import { configureStore } from "@reduxjs/toolkit";
 
-const result = combineReducers({
-    todoReducer,
-    noteReducer
+export const store = configureStore({
+    reducer: {
+        todoReducer,
+        noteReducer
+    }
 });
-
-export const store = redux.createStore(result);
